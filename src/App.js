ee4754b9262d547d96a6ea460e7b35a7591156e9
Laurent FONTAINE <79595818+laurentfontaine1967@users.noreply.github.com>
2021-08-05 09:26:19 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import HeaderPage from './Components/Header';
 import ListePage from './Components/Liste';
-import Supprimer from './Components/Supprimer_user';
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
@@ -13,7 +13,7 @@ function App() {
 
 <HeaderPage/>
 <Route path="/Liste" component={ListePage} />
-<Route path="/Supprimer" component={Supprimer} />
+
 </Router>
 
 
